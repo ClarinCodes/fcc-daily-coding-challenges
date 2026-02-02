197d24a032a28869f56954b9921443b5ee4103e7
Shadow-x <255379724+ClarinAntto@users.noreply.github.com>
2026-02-02 20:32:07 +0530

    Add groundhog_day_prediction function

---
+++ Python/02-02-2026 | Groundhog Day.py
@@ -0,0 +1,9 @@
+def groundhog_day_prediction(appearance):
+    if(appearance == True):
+        return "Looks like we'll have six more weeks of winter."
+    elif(appearance == False):
+        return "It's going to be an early spring."
+    else:
+        return "No prediction this year."
+
+    return appearance
