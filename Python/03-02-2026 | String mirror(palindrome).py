b45700bbd07d021904986cc2044c80d629a3d9b6
Shadow-x <255379724+ClarinAntto@users.noreply.github.com>
2026-02-03 20:50:07 +0530

    3 ways to create string palindrome

---
+++ Python/03-02-2026 | String mirror(palindrome).py
@@ -0,0 +1,12 @@
+def mirror(string):
+  return string + string[::-1]
+
+'''def mirror(string):
+    reverse_str = string[::-1]
+    
+    string = string + reverse_str
+        (or)
+    temp = string + reverse_str
+    string = temp + reverse_str
+    
+    return string '''
