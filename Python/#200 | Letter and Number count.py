286f56b822f830a0c79b529f6245a71926f597f4
Shadow-x <255379724+ClarinAntto@users.noreply.github.com>
2026-02-27 13:43:48 +1200

    Update #200 | Letter and Number count.py

--- Python/#200 | Letter and Number count.py
+++ Python/#200 | Letter and Number count.py
@@ -1,3 +1,12 @@
+#26-02-2026 | 27-02-2025
+
+''' given: a string with a combination of char and int
+    problem: seperate char and string and count. skip null, punctuation and etc. need only char and int.
+    return: count of each and singular or plural
+    example: count_letters_and_numbers("helloworld123") should return "The string has 10 letters and 3 numbers.".
+'''
+
+
 def count_letters_and_numbers(s):
     letters = []
     numbers = []
