53e8ce87ba188341781dda84736a66f81c7d76d6
Shadow-x <255379724+ClarinAntto@users.noreply.github.com>
2026-03-06 13:21:49 +1200

    Update #085 | Word counter.py

--- Python/#085 | Word counter.py
+++ Python/#085 | Word counter.py
@@ -1,5 +1,10 @@
 #03-11-2025 | 09-02-2026
 
+''' given: sentence
+    problem: split the sentence and count the strings
+    return: number of strings present in that sentence
+'''
+
 def count_words(sentence):
     #words=sentence.split()
     return len(sentence.split())
