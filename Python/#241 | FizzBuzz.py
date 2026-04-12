586f9c196e28ad8d9c7a924fe2ab876983398c6e
ClarinAntto <clarinantto1@gmail.com>
2026-04-12 14:15:42 +0530

    added statement lines

--- Python/#241 | FizzBuzz.py
+++ Python/#241 | FizzBuzz.py
@@ -1,5 +1,16 @@
 #08-04-2026 | 08-04-2026
 
+'''
+    Args: lst (list) — an array of integers and/or strings representing the sequence.
+    Problem: from the given list,
+                - multiples of 3 are replaced with "Fizz",
+                - multiples of 5 with "Buzz",
+                - multiples of both with "FizzBuzz",
+            determine whether the array represents a valid FizzBuzz sequence.
+    Return: True if the array is a valid FizzBuzz sequence, otherwise return False.
+'''
+
+
 def is_fizz_buzz(lst):
     for start in range(1, 200):
         correct = True
