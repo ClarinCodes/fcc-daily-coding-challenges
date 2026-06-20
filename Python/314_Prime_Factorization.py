096590250d9a883e216781492c3944df07ee0400
ClarinCodes <clarinantto1@gmail.com>
2026-06-20 22:33:52 +0530

    Create 314_Prime_Factorization.py

---
+++ Python/314_Prime_Factorization.py
@@ -0,0 +1,13 @@
+# 20-06-2026 | 20-06-2026
+
+def prime_factorization(n):
+    factors = []
+    divisor = 2
+
+    while n > 1:
+        while n % divisor == 0:
+            factors.append(divisor)
+            n //= divisor
+        divisor += 1
+
+    return factors
