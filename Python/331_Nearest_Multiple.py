ad62d121d1f7b43ec86bbacbc26a0c54cab80f6a
ClarinCodes <clarinantto1@gmail.com>
2026-07-07 22:34:03 +0530

    Create 331_Nearest_Multiple.py

---
+++ Python/331_Nearest_Multiple.py
@@ -0,0 +1,7 @@
+# 07-07-2026 | 07-07-2026
+
+# Given two integers, round the first to the nearest multiple of the second.
+
+def round_to_nearest_multiple(num, multiple):
+
+    return round(num / multiple) * multiple
